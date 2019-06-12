ccfcd5d9363eac4759f109707140d43be13ded9f
David laiymani <david.laiymani@gmail.com>
2019-06-12 13:47:29 +0200

    Postal UI v1

---
+++ NASAApp/NASAApp/Controllers/PostalCardController.swift
@@ -0,0 +1,30 @@
+//
+//  PostalCardController.swift
+//  NASAApp
+//
+//  Created by davidlaiymani on 12/06/2019.
+//  Copyright © 2019 davidlaiymani. All rights reserved.
+//
+
+import UIKit
+
+class PostalCardController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
