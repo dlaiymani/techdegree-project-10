a6edbe44e994ca2e28707dce60ce5655fbb15006
David laiymani <david.laiymani@gmail.com>
2019-06-13 13:41:11 +0200

    New UI for postcard

--- NASAApp/NASAApp/Controllers/PostalCardController.swift
+++ NASAApp/NASAApp/Controllers/PostalCardController.swift
@@ -13,9 +13,10 @@ class PostalCardController: UIViewController {
     var roverPhoto: RoverPhoto?
     
     @IBOutlet weak var roverImageView: UIImageView!
-    @IBOutlet weak var colorSegmentedControl: UISegmentedControl!
     @IBOutlet weak var userText: UITextField!
     @IBOutlet weak var userLabel: UILabel!
+    
+    @IBOutlet var colorButtons: [UIButton]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,8 +27,11 @@ class PostalCardController: UIViewController {
             roverImageView.image = roverPhoto.roverImage
             userLabel.text = roverPhoto.photoDate
             
-            userLabel.transform = CGAffineTransform(rotationAngle: -CGFloat.pi / 4)
-
+            for button in colorButtons {
+                button.layer.cornerRadius = button.frame.width/2
+                button.layer.masksToBounds = true
+            }
+            
         }
     }
     
@@ -36,20 +40,44 @@ class PostalCardController: UIViewController {
         userLabel.text = sender.text
     }
     
-    @IBAction func colorChanged(_ sender: Any) {
-        switch colorSegmentedControl.selectedSegmentIndex {
+    @IBAction func colorChanged(_ sender: UIButton) {
+        switch sender.tag {
         case 0:
-            userLabel.textColor = .red
+            userLabel.textColor = UIColor(red: 0.888, green: 0.375, blue: 0.366, alpha: 1.0)
         case 1:
-            userLabel.textColor = .blue
+            userLabel.textColor = UIColor(red: 0.0, green: 0.563, blue: 0.319, alpha: 1)
         case 2:
-            userLabel.textColor = .green
+            userLabel.textColor = UIColor(red: 0, green: 0.590, blue: 1.0, alpha: 1)
         case 3:
-            userLabel.textColor = .yellow
+            userLabel.textColor = UIColor(red: 1.0, green: 0.832, blue: 0.473, alpha: 1)
+        case 4:
+            userLabel.textColor = UIColor(red: 0.845, green: 0.515, blue: 1.0, alpha: 1)
+        case 5:
+                userLabel.textColor = .black
         default:
             userLabel.textColor = .black
         }
     }
+    
+    
+    @IBAction func sharedButtonTapped(_ sender: UIButton) {
+        
+        let newImage = addTextToImage(text: userText.text!, inImage: roverImageView.image!, atPoint: CGPoint(x: 0, y: 0))
+        roverImageView.image = newImage
+        
+        
+        let shareViewController = UIActivityViewController(activityItems: [newImage, "hello"], applicationActivities: nil)
+
+     //   let shareViewController = UIActivityViewController(activityItems: [newImage], applicationActivities: nil)
+        present(shareViewController, animated: true, completion: nil)
+        if let popOver = shareViewController.popoverPresentationController {
+            popOver.sourceView = self.view
+            popOver.barButtonItem = self.navigationItem.rightBarButtonItem;
+
+           // popOver.sourceRect = popOver.barButtonItem
+        }
+    }
+    
     
     func addTextToImage(text: String, inImage: UIImage, atPoint:CGPoint) -> UIImage? {
     
@@ -67,16 +95,23 @@ class PostalCardController: UIViewController {
         let attributes = font != nil ? [NSAttributedString.Key.font: font, NSAttributedString.Key.foregroundColor : color] : [:]
         let textWidth = text.size(withAttributes: attributes).width
         let textHeight = text.size(withAttributes: attributes).height
+       
         
         let textRect = CGRect(x: drawingBounds.size.width/2 - textWidth/2, y: drawingBounds.size.height/2 - textHeight/2,
                               width: textWidth, height: textHeight)
         
+        
+        userLabel.text = ""
         text.draw(in: textRect, withAttributes: attributes)
+
         // Get the image from the graphics context
         let newImag = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return newImag
     }
+    
+    
+    
 
 }
 
@@ -84,9 +119,6 @@ extension PostalCardController: UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         self.userText.resignFirstResponder()
-
-        let newImage = addTextToImage(text: userText.text!, inImage: roverImageView.image!, atPoint: CGPoint(x: 0, y: 0))
-        roverImageView.image = newImage
         
         return true
     }
