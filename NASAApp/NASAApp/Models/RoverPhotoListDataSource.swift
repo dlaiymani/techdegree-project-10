407a031d54063277efc5477615af7eafd27f2d76
David laiymani <david.laiymani@gmail.com>
2019-06-17 14:08:09 +0200

    Unit test

--- NASAApp/NASAApp/Models/RoverPhotoListDataSource.swift
+++ NASAApp/NASAApp/Models/RoverPhotoListDataSource.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 import Alamofire
-import AlamofireImage
+//import AlamofireImage
 
 class RoverPhotoListDataSource: NSObject, UICollectionViewDataSource {
     
---
+++ NASAApp/Pods/Target Support Files/Pods-NASAAppTests/Pods-NASAAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NASAAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_NASAAppTests
+@end
