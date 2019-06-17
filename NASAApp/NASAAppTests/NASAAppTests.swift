63d75f67385c0e76fb083a9bd914eaaa145c0db8
David laiymani <david.laiymani@gmail.com>
2019-06-17 14:35:28 +0200

    First unit test

--- NASAApp/NASAAppTests/NASAAppTests.swift
+++ NASAApp/NASAAppTests/NASAAppTests.swift
@@ -7,28 +7,51 @@
 //
 
 import XCTest
-@testable import APIClient
+@testable import NASAApp
 
 class NASAAppTests: XCTestCase {
 
+    var roverAPIClient: APIClient!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        roverAPIClient = APIClient()
     }
 
+    func testValidCallToMarsRover() {
+        let roverUrl = URL(string: "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2019-05-05&page=1&api_key=abONaFIip0FrAmEcZLiXbZqIUw2r7dOUPmRFWZMN")
+        
+        let promise = expectation(description: "Status code: 200")
+
+        roverAPIClient.execute(roverUrl!) { (jsonData, error) in
+            if let error = error {
+                XCTFail("Error: \(error.localizedDescription)")
+            } else {
+                if let jsonData = jsonData {
+                    let decoder = JSONDecoder()
+                    let photos = try! decoder.decode([String: [RoverPhoto]].self, from: jsonData)
+                    let roverPhotos = photos["photos"]!
+                    if roverPhotos.count == 0 {
+                       // self.showAlert(withTitle: "No Photo", message: "Please try to change the date")
+                        XCTFail("Error: No Photo")
+                    } else {
+                      promise.fulfill()
+                    }
+                } else {
+                   XCTFail("Error: Bad JSON format")
+                }
+            }
+        }
+        wait(for: [promise], timeout: 5)
+    }
+    
     override func tearDown() {
+        roverAPIClient = nil
+        super.tearDown()
+        
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    
 
 }
